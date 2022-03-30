9aa02072c66979437841367aa875d2c47ec9e4cb
Kevin Bui <kvb2univpitt@gmail.com>
2022-03-30 13:58:20 -0400

    Added methods to check for admin role and session expiration.

--- ontology-store-ws/src/main/java/edu/pitt/dbmi/ontology/store/ws/db/PmDBAccess.java
+++ ontology-store-ws/src/main/java/edu/pitt/dbmi/ontology/store/ws/db/PmDBAccess.java
@@ -18,6 +18,12 @@
  */
 package edu.pitt.dbmi.ontology.store.ws.db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import javax.sql.DataSource;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -29,10 +35,72 @@ import org.springframework.jdbc.core.JdbcTemplate;
  */
 public class PmDBAccess {
 
+    private static final String ADMIN_ROLE_SQL_TEMPLATE = "SELECT DISTINCT ppur.user_role_cd "
+            + "FROM %s.pm_user_data pud "
+            + "LEFT JOIN %s.pm_project_user_roles ppur ON pud.user_id = ppur.user_id AND ppur.status_cd <> 'D' AND ppur.user_role_cd = 'ADMIN' "
+            + "WHERE pud.user_id = ?";
+    private static final String SESSION_EXPIRATION_SQL_TEMPLATE = "SELECT expired_date FROM %s.pm_user_session WHERE user_id = ? AND session_id = ?";
+
     private final JdbcTemplate jdbcTemplate;
 
     public PmDBAccess(DataSource dataSource) {
         this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public boolean isAdmin(String userId) throws SQLException {
+        DataSource dataSource = jdbcTemplate.getDataSource();
+        if (dataSource != null) {
+            try (Connection conn = dataSource.getConnection()) {
+                String sql = String.format(ADMIN_ROLE_SQL_TEMPLATE, conn.getSchema(), conn.getSchema());
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                stmt.setString(1, userId);
+
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()) {
+                    String role = rs.getString("user_role_cd");
+                    if (role != null && role.trim().toLowerCase().equals("admin")) {
+                        return true;
+                    }
+                }
+
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasExpiredSession(String username, String password) throws SQLException {
+        LocalDateTime expiration = getSessionExpiration(username, password);
+        if (expiration == null) {
+            return true;
+        }
+
+        return LocalDateTime.now().compareTo(expiration) > 0;
+    }
+
+    private LocalDateTime getSessionExpiration(String username, String password) throws SQLException {
+        LocalDateTime expiration = null;
+
+        DataSource dataSource = jdbcTemplate.getDataSource();
+        if (dataSource != null) {
+            try (Connection conn = dataSource.getConnection()) {
+                String sql = String.format(SESSION_EXPIRATION_SQL_TEMPLATE, conn.getSchema());
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                stmt.setString(1, username);
+                stmt.setString(2, password);
+
+                ResultSet resultSet = stmt.executeQuery();
+                while (resultSet.next()) {
+                    Timestamp expirationDate = resultSet.getTimestamp("expired_date");
+                    if (expirationDate != null) {
+                        expiration = expirationDate.toLocalDateTime();
+                    }
+                }
+
+            }
+        }
+
+        return expiration;
     }
 
 }
