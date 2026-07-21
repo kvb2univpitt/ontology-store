47968b1b4b1ae58ecaf2df77394c35733df8b8dc
Kevin Bui <kvb2univpitt@gmail.com>
2026-07-20 23:21:02 -0400

    Updated sample db scripts for reusing data source.

--- sample_setup/oracle/ontstore_ds_xml.sql
+++ sample_setup/oracle/ontstore_ds_xml.sql
@@ -2,6 +2,32 @@
 -- Demo Project Ontologystore
 -- ################################################################################
 
+-- CREATE USER i2b2ontstoredata IDENTIFIED BY demouser;
+-- CREATE USER i2b2ontstoremetadata IDENTIFIED BY demouser;
+
+-- GRANT ALL PRIVILEGES TO i2b2ontstoredata;
+-- GRANT ALL PRIVILEGES TO i2b2ontstoremetadata;
+
+-- GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE ON SCHEMA i2b2demodata TO i2b2ontstoredata;
+-- GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE ON SCHEMA i2b2metadata TO i2b2ontstoremetadata;
+
+-- ################################################################################
+-- ACT Project Ontologystore
+-- ################################################################################
+
+-- CREATE USER i2b2ontstoreactdata IDENTIFIED BY demouser;
+-- CREATE USER i2b2ontstoreactmetadata IDENTIFIED BY demouser;
+
+-- GRANT ALL PRIVILEGES TO i2b2ontstoreactdata;
+-- GRANT ALL PRIVILEGES TO i2b2ontstoreactmetadata;
+
+-- GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE ON SCHEMA i2b2demodata TO i2b2ontstoreactdata;
+-- GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE ON SCHEMA i2b2actata TO i2b2ontstoreactmetadata;
+
+-- ################################################################################
+-- Demo and ACT Project Ontologystore
+-- ################################################################################
+
 CREATE USER i2b2ontstoredata IDENTIFIED BY demouser;
 CREATE USER i2b2ontstoremetadata IDENTIFIED BY demouser;
 
@@ -10,16 +36,4 @@ GRANT ALL PRIVILEGES TO i2b2ontstoremetadata;
 
 GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE ON SCHEMA i2b2demodata TO i2b2ontstoredata;
 GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE ON SCHEMA i2b2metadata TO i2b2ontstoremetadata;
-
--- ################################################################################
--- ACT Project Ontologystore
--- ################################################################################
-
-CREATE USER i2b2ontstoreactdata IDENTIFIED BY demouser;
-CREATE USER i2b2ontstoreactmetadata IDENTIFIED BY demouser;
-
-GRANT ALL PRIVILEGES TO i2b2ontstoreactdata;
-GRANT ALL PRIVILEGES TO i2b2ontstoreactmetadata;
-
-GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE ON SCHEMA i2b2demodata TO i2b2ontstoreactdata;
-GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE ON SCHEMA i2b2actata TO i2b2ontstoreactmetadata;
+GRANT SELECT ANY TABLE, INSERT ANY TABLE, UPDATE ANY TABLE ON SCHEMA i2b2actata TO i2b2ontstoremetadata;
--- sample_setup/sqlserver/ontstore_ds_xml.sql
+++ sample_setup/sqlserver/ontstore_ds_xml.sql
@@ -1,3 +1,54 @@
+-- ################################################################################
+-- Demo Project Ontologystore
+-- ################################################################################
+
+-- -- create crc user
+-- USE i2b2demodata
+
+-- CREATE LOGIN i2b2ontstoredata WITH PASSWORD = '<YourStrong@Passw0rd>';
+
+-- CREATE USER i2b2ontstoredata FOR LOGIN i2b2ontstoredata
+
+-- GRANT CREATE TABLE TO i2b2ontstoredata;
+-- GRANT ALTER,SELECT,INSERT,UPDATE ON SCHEMA :: dbo TO i2b2ontstoredata;
+
+-- -- create metadata user
+-- USE i2b2metadata
+-- CREATE LOGIN i2b2ontstoremetadata WITH PASSWORD = '<YourStrong@Passw0rd>';
+
+-- CREATE USER i2b2ontstoremetadata FOR LOGIN i2b2ontstoremetadata
+
+-- GRANT CREATE TABLE TO i2b2ontstoremetadata;
+-- GRANT ALTER,SELECT,INSERT,UPDATE ON SCHEMA :: dbo TO i2b2ontstoremetadata;
+
+-- ################################################################################
+-- ACT Project Ontologystore
+-- ################################################################################
+
+-- -- create ACT crc user
+-- USE i2b2demodata
+
+-- CREATE LOGIN i2b2ontstoreactdata WITH PASSWORD = '<YourStrong@Passw0rd>';
+
+-- CREATE USER i2b2ontstoreactdata FOR LOGIN i2b2ontstoreactdata
+
+-- GRANT CREATE TABLE TO i2b2ontstoreactdata;
+-- GRANT ALTER,SELECT,INSERT,UPDATE ON SCHEMA :: dbo TO i2b2ontstoreactdata;
+
+-- -- create ACT metadata user
+-- USE i2b2actata
+
+-- CREATE LOGIN i2b2ontstoreactmetadata WITH PASSWORD = '<YourStrong@Passw0rd>';
+
+-- CREATE USER i2b2ontstoreactmetadata FOR LOGIN i2b2ontstoreactmetadata
+
+-- GRANT CREATE TABLE TO i2b2ontstoreactmetadata;
+-- GRANT ALTER,SELECT,INSERT,UPDATE ON SCHEMA :: dbo TO i2b2ontstoreactmetadata;
+
+-- ################################################################################
+-- Demo and ACT Project Ontologystore
+-- ################################################################################
+
 -- create crc user
 USE i2b2demodata
 
@@ -17,23 +68,9 @@ CREATE USER i2b2ontstoremetadata FOR LOGIN i2b2ontstoremetadata
 GRANT CREATE TABLE TO i2b2ontstoremetadata;
 GRANT ALTER,SELECT,INSERT,UPDATE ON SCHEMA :: dbo TO i2b2ontstoremetadata;
 
-
--- create ACT crc user
-USE i2b2demodata
-
-CREATE LOGIN i2b2ontstoreactdata WITH PASSWORD = '<YourStrong@Passw0rd>';
-
-CREATE USER i2b2ontstoreactdata FOR LOGIN i2b2ontstoreactdata
-
-GRANT CREATE TABLE TO i2b2ontstoreactdata;
-GRANT ALTER,SELECT,INSERT,UPDATE ON SCHEMA :: dbo TO i2b2ontstoreactdata;
-
 -- create ACT metadata user
 USE i2b2actata
+CREATE USER i2b2ontstoremetadata FOR LOGIN i2b2ontstoremetadata
 
-CREATE LOGIN i2b2ontstoreactmetadata WITH PASSWORD = '<YourStrong@Passw0rd>';
-
-CREATE USER i2b2ontstoreactmetadata FOR LOGIN i2b2ontstoreactmetadata
-
-GRANT CREATE TABLE TO i2b2ontstoreactmetadata;
-GRANT ALTER,SELECT,INSERT,UPDATE ON SCHEMA :: dbo TO i2b2ontstoreactmetadata;
+GRANT CREATE TABLE TO i2b2ontstoremetadata;
+GRANT ALTER,SELECT,INSERT,UPDATE ON SCHEMA :: dbo TO i2b2ontstoremetadata;
